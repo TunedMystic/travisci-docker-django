e140baca99ec38d98da5e354ce677220bdfd991f
Sandeep Jadoonanan <jsanweb@gmail.com>
2018-01-12 19:20:00 -0500

    Create Company tests with fixtures.

--- app/companies/tests.py
+++ app/companies/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Company
+
+
+class CompaniesTestCase(TestCase):
+    fixtures = ['companies.json', 'users.json']
+
+    def test_company_legal_name(self):
+        for company in Company.objects.all():
+            self.assertEqual(company.legal_name, company.name.upper())
