e4789c912428b10a74a1c61124fe25a46e57befb
Sandeep Jadoonanan <jsanweb@gmail.com>
2018-01-12 19:56:27 -0500

    Create more company tests.

--- app/companies/tests.py
+++ app/companies/tests.py
@@ -9,3 +9,11 @@ class CompaniesTestCase(TestCase):
     def test_company_legal_name(self):
         for company in Company.objects.all():
             self.assertEqual(company.legal_name, company.name.upper())
+
+    def test_company_creation(self):
+        data = {
+            'name': 'TEST-COMPANY',
+            'address': '5 Main street'
+        }
+        company = Company.objects.create(**data)
+        self.assertEqual(company.name, data['name'])
